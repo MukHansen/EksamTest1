801ef453b33f3fd38a2b6068a21db7a805081291
Mads <cph-mh752@cphbusiness.dk>
2020-01-07 16:22:48 +0100

    Initial Travis.ci Setup

--- src/main/java/utils/EMF_Creator.java
+++ src/main/java/utils/EMF_Creator.java
@@ -118,7 +118,7 @@ public class EMF_Creator {
         if (isDeployed) {
             user = System.getenv("USER");
             pw = System.getenv("PW");
-            connection_str = System.getenv("CONNECTION_CA3_MIN");
+            connection_str = System.getenv("CONNECTION_EksamTest1");
         }
         /*
         On your server in /opt/tomcat/bin/setenv.sh   add the following WITH YOUR OWN VALUES
